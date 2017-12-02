7dc596d9415f9beee102bc8b064b94e23612097c
Mark Shelton <mark@shelton.id.au>
2017-12-02 11:37:54 +0800

    added empty commonenv

--- installzsh.sh
+++ installzsh.sh
@@ -3,4 +3,4 @@ apt-get install zsh
 apt-get install git-core
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 chsh -s `which zsh`
-sudo shutdown -r 0
+shutdown -r 0
